b4c06aecd68970f4dcea40ad232b1a92db0d2c57
Johnny <3202241012@stu.xaut.edu.cn>
2021-09-24 08:12:46 +0800

    initial commit

---
+++ prac_03/broken_score.py
@@ -0,0 +1,29 @@
+import random
+
+
+def main():
+    score = take_in_scores()
+    if score < 0:
+        print("Invalid score")
+    else:
+        if score > 100:
+            print("Invalid score")
+        if 50 <= score <= 90:
+            print("Passable and your score is {}".format(score))
+        if score > 90:
+            print("Excellent and your score is {}".format(score))
+        if score < 50:
+            print("Bad and your score is {}".format(score))
+
+
+def take_in_scores():
+    score = random_number()
+    return score
+
+
+def random_number():
+    number = random.randint(0, 100)
+    return number
+
+
+main()
